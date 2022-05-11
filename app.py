e87a1e4252227f39871ff06ae86f519337a8b25b
changyeop <changyeop@comcom.ai>
2022-05-11 18:21:36 +0900

    feat: get params

---
+++ app.py
@@ -0,0 +1,62 @@
+import argparse
+
+
+def get_args() -> argparse.Namespace:
+    """
+    Get parameters for script execution.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--server_name",
+        type=str,
+        required=True,
+        help="name that identifies the server",
+    )
+    parser.add_argument(
+        "--webhook_url",
+        type=str,
+        required=True,
+        help="slack webhook url",
+    )
+    parser.add_argument(
+        "--interval",
+        type=int,
+        default=60,
+        help="interval to check gpu usage (second)",
+    )
+    parser.add_argument(
+        "--utilization_threshold", type=int, default=40, help="minimum GPU utilization"
+    )
+    parser.add_argument(
+        "--time_threshold",
+        type=int,
+        default=60 * 60,  # 1 hour
+        help="time to start sending danger alarms",
+    )
+    return parser.parse_args()
+
+
+def check_args(args: argparse.Namespace) -> None:
+    """
+    Checks whether the input parameters are valid.
+    If it is an invalid parameter, an error is raised.
+    """
+    if args.interval < 0:
+        raise ValueError("The value of `interval` must be positive.")
+    if args.interval < args.time_threshold:
+        raise ValueError(
+            "The value of `time_threshold` must be greater than the value of `interval`."
+        )
+    if args.utilization_threshold < 0 or args.utilization_threshold > 100:
+        raise ValueError("The value of `utilization_threshold` must be between 0 and 100.")
+
+
+def main(args: argparse.Namespace) -> None:
+    """
+    Main function
+    """
+    check_args(args)
+
+
+if __name__ == "__main__":
+    main(get_args)
