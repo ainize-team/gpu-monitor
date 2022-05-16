aed404b3865bb586c17179ca399728fde0e71fb8
YoungJae Kim <byeongal@comcom.ai>
2022-05-16 18:58:31 +0900

    add Docker file and more log

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import argparse
@@ -11,6 +12,7 @@ from constants import ExitStatusEnum, SlackMessageTypeEnum
 
 low_utilization_start_time = None
 
+
 def get_args() -> argparse.Namespace:
     """
     Get parameters for script execution.
@@ -19,28 +21,31 @@ def get_args() -> argparse.Namespace:
     parser.add_argument(
         "--server_name",
         type=str,
-        required=True,
+        default=os.getenv("SERVER_NAME", None),
         help="name that identifies the server",
     )
     parser.add_argument(
         "--webhook_url",
         type=str,
-        required=True,
+        default=os.getenv("WEBHOOK_URL", None),
         help="slack webhook url",
     )
     parser.add_argument(
         "--interval",
         type=int,
-        default=60,
+        default=int(os.getenv("INTERVAL", 60)),
         help="interval to check GPU usage (second)",
     )
     parser.add_argument(
-        "--utilization_threshold", type=int, default=40, help="minimum GPU utilization"
+        "--utilization_threshold",
+        type=int,
+        default=int(os.getenv("UTILIZATION_THRESHOLD", 40)),
+        help="minimum GPU utilization",
     )
     parser.add_argument(
         "--time_threshold",
         type=int,
-        default=60 * 60,  # 1 hour
+        default=int(os.getenv("TIME_THRESHOLD", 3600)),
         help="time to start sending danger alarms",
     )
     return parser.parse_args()
@@ -61,7 +66,9 @@ def check_args(args: argparse.Namespace) -> None:
         raise ValueError("The value of `utilization_threshold` must be between 0 and 100.")
 
 
-def _gpu_check_job(utilization_threshold: int, server_name: str, time_threshold:int, slack_bot) -> None:
+def _gpu_check_job(
+    utilization_threshold: int, server_name: str, time_threshold: int, slack_bot
+) -> None:
     """
     Job to get GPU information every interval
 
@@ -70,13 +77,17 @@ def _gpu_check_job(utilization_threshold: int, server_name: str, time_threshold:
         time_threshold (int): time to start sending danger alarms
     """
     global low_utilization_start_time
+    now = time.time()
+    logger.info("Check GPU")
     gpu_information_list = get_gpus()
     average_gpu_utilization = get_average_gpu_utilization(gpu_information_list)
+    logger.info(f"GPU Utilization : {average_gpu_utilization}")
     if average_gpu_utilization <= utilization_threshold:
-        now = time.time()
         if low_utilization_start_time is None:
+            logger.warning("GPU Utilization is low")
             low_utilization_start_time = now
         elif now - low_utilization_start_time >= time_threshold:
+            logger.error("GPU Utilization is low")
             low_utilization_start_time = None
             slack_bot.send_message(
                 SlackMessageTypeEnum.ERROR_MESSAGE.value, server_name, average_gpu_utilization
@@ -107,7 +118,7 @@ def main(args: argparse.Namespace) -> None:
     scheduler.add_job(
         _gpu_check_job,
         "interval",
-        args=[args.utilization_threshold, args.server_name,args.time_threshold, slack_bot],
+        args=[args.utilization_threshold, args.server_name, args.time_threshold, slack_bot],
         seconds=args.interval,
     )
     scheduler.start()
